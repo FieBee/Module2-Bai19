e3f203d5732ea872486d00d0f4254a3648baa477
FieBee <phuonglf98@gmail.com>
2022-07-27 20:52:06 +0700

    add BT-ValidateClassName

---
+++ BT-ValidateClassName/src/ClassNameClient.java
@@ -0,0 +1,32 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ClassNameClient {
+    public static void main(String[] args) {
+        try{
+            String c1 = "C0318G";
+            String c2 = "M0318G";
+            String c3 = "P0323A";
+
+            String regex = "^[CAP]\\d{4}[G-K]$";
+
+            Pattern pattern = Pattern.compile(regex);
+            Matcher m1 = pattern.matcher(c1);
+            Matcher m2 = pattern.matcher(c2);
+            Matcher m3 = pattern.matcher(c3);
+
+            boolean matcher1 = m1.matches();
+            boolean matcher2 = m2.matches();
+            boolean matcher3 = m3.matches();
+
+            System.out.println("Match = " + matcher1);
+            System.out.println("Match = " + matcher2);
+            System.out.println("Match = " + matcher3);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
